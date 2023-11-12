ff430fd8280e1e655b4e66d8a10e80bf2f563c4d
Letlhogonolo Segoe <noloxtreme@gmail.com>
2023-11-12 13:41:01 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+from dotenv import load_dotenv
 from flask import Flask, request
-from langchain.document_loaders.text import TextLoader
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.vectorstores.faiss import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.prompts import ChatPromptTemplate
-from langchain.chat_models import ChatOpenAI
-from dotenv import load_dotenv
+
 
 # Press the green button in the gutter to run the script.
 
 def loadVectorStore():
-    loader = TextLoader(file_path="D:\OpenAI\MyData\JiraAPI.txt")
+    loader = DirectoryLoader("D:\OpenAI\MyData", glob="**/*.txt", show_progress=True)
     documents = loader.load()
 
     embeddings = OpenAIEmbeddings()
@@ -49,7 +50,7 @@ chain = (
 
 app = Flask(__name__)
 
-@app.route('/query-my-data', methods=['POST'],mimetype='text/plain')
+@app.route('/query-my-data', methods=['POST'])
 def post():
     data = request.get_json()
     return chain.invoke(data['question'])
